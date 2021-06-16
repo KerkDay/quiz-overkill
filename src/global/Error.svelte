a25fdba7b0015c852a3ffbbbaf500f0a652d7ebc
Kerk Day <KerkDay@outlook.com>
2021-06-15 23:15:47 -0600

    Initial init of Error component. Haven't thought of when to use it though 🤷

---
+++ src/global/Error.svelte
@@ -0,0 +1,27 @@
+<script>
+
+let error = null
+
+onMount(()=>{
+  setTimeout(()=> {
+    error = null
+  }, 5000)
+})
+
+</script>
+
+{#if error}
+  <div class='error'>{$error}</div>
+{/if}
+
+<style>
+.error {
+  position: fixed;
+  bottom: 0; left: 0; right: 0;
+  font-size: .75rem;
+  background: var(--red);
+  color: var(--white);
+  padding: .5em;
+  opacity: .75;
+}
+</style>
