3a637a846ee27ba46867ad46441f1b9493785dfb
Kerk Day <KerkDay@outlook.com>
2021-06-19 20:59:33 -0600

    Add default values to the options

--- src/stores.js
+++ src/stores.js
@@ -2,6 +2,24 @@
 	// Import Svelte Stuff
 	import {writable} from 'svelte/store'
 	// Create the 'Stores' that will act as all the data needed for this SPA
-	export const optStore = writable({})
+
+	/* DEFAAULT OPTIONS */
+	export const optStore = writable({
+		// Main
+		title: "Quiz", 
+		description: "", 
+		welcomeImg: null, 
+
+		// Quiz Options
+		qSelection: "ai", // How to select a question // ai, ordered, random
+		canGoBack: true, // Can a user go back after answering a question
+		canSkip: true, // Can a user skip a question
+		tieBreakers: "random", // How to handle tied results // random, show all
+		minQuestions: 0, // Minimum amount of questions before showing results
+		transition: "fade", // Transition between questions // none, fade, horizontal, vertical
+
+		// Editor Options
+		idLength: 2 // length of ID codes.
+	})
 	export const charStore = writable([])
 	export const qStore = writable([])
