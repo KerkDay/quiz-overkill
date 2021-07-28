5ef570b4160b08887bf336bd71b976118c4e0c50
Kerk Day <kerkday@outlook.com>
2021-07-27 19:26:17 -0600

    FFMPEG doesn't like when an image has an odd number in it's scaled size. This code makes FFMPEG happy. Also, Images are stored as Object URLs, just until I figure out exactly how I want to store them in the users browser.

--- src/scripts/imageCompress.js
+++ src/scripts/imageCompress.js
@@ -7,12 +7,31 @@ export default async function compressImg(img, size = 300) {
   if (img) {
     /* GIF or Video */
     if (img.type === 'image/gif') {
+
+      const promise = new Promise((resolve, reject) => {
+        const i = new Image()
+        i.onload = function() { 
+          let w = this.width, h = this.height
+          let big = Math.max(w, h)
+          w = Math.floor(w * size/big)
+          h = Math.floor(h * size/big)
+
+          h = h%2 === 0 ? h : h+1
+          w = w%2 === 0 ? w : w+1
+
+          resolve({w, h}) 
+        }
+        i.src = URL.createObjectURL(img)
+      })
+      let {w, h} = await promise
+      console.log(`[GIF Image Size] ${w} x ${h}`)
+
       ffmpeg.isLoaded() ? "" : await ffmpeg.load();
       ffmpeg.FS('writeFile', 'convert.gif', await fetchFile(img))
 
       await ffmpeg.run(
         '-i', 'convert.gif', 
-        '-vf', `scale='min(${size},iw)':'min(${size},ih)'`, 
+        '-vf', `scale=${w}:${h}`, 
         '-pix_fmt', 'yuv420p', 
         '-f', 'mp4', 
         'out.mp4'
@@ -36,7 +55,8 @@ export default async function compressImg(img, size = 300) {
           maxWidthOrHeight: size,
           useWebWorker: true
         })
-        let url = await imageCompression.getDataUrlFromFile(compressedImg)
+        let buffer = await compressedImg.arrayBuffer()
+        let url = URL.createObjectURL(new Blob([buffer], {type: img.type}))
 
         let results = {
           type: 'image',
