67e267e279127b55eea0870ad651906900cc0400
Kerk Day <kerkday@outlook.com>
2021-08-15 17:06:13 -0600

    Allowing preprocessor in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
+import preprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
