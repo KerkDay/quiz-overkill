186ede40064ca298ececb30e7422005bc1cba89b
Kerk Day <kerkday@outlook.com>
2021-08-15 21:36:29 -0600

    Why did I add this?

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import crossOriginIsolation from 'vite-plugin-cross-origin-isolation'
-import preprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
